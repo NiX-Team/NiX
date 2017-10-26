e28552ce06db509cfb1781ee2174021e4e413bd8
Yotau Hong <crazymousethief@outlook.com>
2017-10-26 21:29:24 +0800

    Add support for react

---
+++ app/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class App extends React.Component {
+  render() {
+    return <h1>NiX Team Blog</h1>
+  }
+}
+
+export default App
---
+++ webpack.config.dev.babel.js
@@ -0,0 +1,58 @@
+import webpack from 'webpack'
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const appPath = path.resolve(process.cwd(), 'app')
+
+export default {
+  context: appPath,
+  target: 'web',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(process.cwd(), 'build'),
+    pathinfo: true,
+    filename: 'static/js/bundle.js',
+    chunkFilename: 'static/js/[name].chunk.js',
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        include: appPath,
+        options: {
+          babelrc: false,
+          presets: ['babel-preset-react-app'],
+          // This is a feature of `babel-loader` for webpack (not Babel itself).
+          // It enables caching results in ./node_modules/.cache/babel-loader/
+          // directory for faster rebuilds.
+          cacheDirectory: true,
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development',
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(appPath, 'public/index.html'),
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    compress: true,
+    port: 3000,
+  },
+}
